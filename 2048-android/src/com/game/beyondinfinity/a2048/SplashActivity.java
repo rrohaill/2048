e6c4112bcc527b3db171df499800977e25a2409c
Rohail Ahmad <rohailonline@hotmail.com>
2017-06-24 17:32:08 +0200

    Game 2048

---
+++ 2048-android/src/com/game/beyondinfinity/a2048/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.game.beyondinfinity.a2048;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    private Thread mSplashThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mSplashThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    synchronized (this) {
+                        // Wait given period of time or exit on touch
+                        wait(4000);
+                    }
+                } catch (InterruptedException ex) {
+                }
+
+                // Run next activity
+                Intent intent = new Intent();
+                intent.setClass(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        };
+        mSplashThread.start();
+
+    }
+}
